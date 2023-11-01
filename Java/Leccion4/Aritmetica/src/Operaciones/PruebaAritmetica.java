e8195a6cd8ba20eae3e8db28ca4a86b3f18ec319
Sofia Lopez <sofiablopezz@hotmail.com>
2023-11-01 05:07:49 -0300

    Commit de Java Clase 5 y 6
    Commit del contenido visto en Java clase 5 y 6

--- Java/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -3,15 +3,35 @@ package Operaciones;
 
 public class PruebaAritmetica {
     public static void main(String[] args) {
+        int a = 10; //variables locales;
+        int b = 7;//llamamos al stack 
+        miMetodo(); //llamamos el método nuevo
         Aritmetica aritmetica1 = new Aritmetica();
         aritmetica1.a = 3;
         aritmetica1.a = 7;
         aritmetica1.sumarNumeros();
-        
+        //Para almacenar un objeto o los atributos se utiliza la memoria heap
         int resultado = aritmetica1.sumarConRetorno();
         System.out.println("resultado = " + resultado);
         
         resultado = aritmetica1.sumarConArgumentos(12, 26);
         System.out.println("resultado = " + resultado);
+        
+        System.out.println("aritmetica1 a:"+aritmetica1.a);
+        System.out.println("aritmetica1 b:"+aritmetica1.b);
+        
+        Aritmetica aritmetica2 = new Aritmetica(5, 0);
+        System.out.println("aritmetica2 = " + aritmetica2.a);
+        System.out.println("aritmetica2 = " + aritmetica2.b);
+        //aritmetica1 = null; nunca utilizar esto, no se debe hacer
+        /*System.gc(); //innecesario,metodo para limpiar residuos,muy pesado. 
+        El compilador lo hace solo-
+        */
+    }
+    
+    public static void miMetodo(){
+        //a = 10; //una variable limitada
+        System.out.println("Aqui hay otro método");
     }
 }
+
