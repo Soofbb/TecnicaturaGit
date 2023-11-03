201ba0d80083b1b3027314cdb2b2655ee8d26fff
Sofia Lopez <sofiablopezz@hotmail.com>
2023-11-02 23:17:44 -0300

    Commit de Java Clase 7 en POO
    Encapsulamiento, clases y objetos

--- Java/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -25,13 +25,43 @@ public class PruebaAritmetica {
         System.out.println("aritmetica2 = " + aritmetica2.b);
         //aritmetica1 = null; nunca utilizar esto, no se debe hacer
         /*System.gc(); //innecesario,metodo para limpiar residuos,muy pesado. 
-        El compilador lo hace solo-
+        El compilador lo hace solo
         */
+        Persona persona = new Persona("Ariel","Betancud");
+        System.out.println("persona = " + persona);
+        System.out.println("Persona nombre: "+persona.nombre); 
+        System.out.println("Persona apellido: "+persona.apellido);
     }
-    
+    //Modularidad, creamos un nuevo método
     public static void miMetodo(){
         //a = 10; //una variable limitada
         System.out.println("Aqui hay otro método");
     }
 }
+//Creamos una nueva clase
+class Persona{
+    String nombre;
+    String apellido;
 
+    Persona(String nombre, String apellido){ //Constructor
+        super(); //Llamada al constructor de la clase Padre object
+        //Imprimir imprimir = new Imprimir();
+        new Imprimir().imprimir(this);
+        this.nombre = nombre;
+        this.apellido = apellido;
+        System.out.println("Objeto persona usando this: "+this);
+    }    
+}
+
+class Imprimir{
+    public Imprimir(){
+        //super(); //El constructor de la clase padre, para reservar memoria
+        Imprimir imprimir = new Imprimir();
+        
+    }
+    
+    public void imprimir(Persona persona){
+        System.out.println("Persona desde la clase imprimir: "+persona);
+        System.out.println("Impresión del objeto actual (this)");
+    }
+}
